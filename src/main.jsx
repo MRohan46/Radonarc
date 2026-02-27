67c34eb5aa5e059fcc270c0126fe3c02603a168f
MRohan46 <rohanmuhammad222k@gmail.com>
2026-02-27 16:29:16 +0500

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import RadonArc from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RadonArc />
+    <>
+      <RadonArc />
+      <Analytics />
+      <SpeedInsights />
+    </>
   </StrictMode>,
 )
